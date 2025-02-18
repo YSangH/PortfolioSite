e23ec434eca9980242dd9469ef9f5dc9bd4bd464
ysh <ysh940129@gmail.com>
2025-02-18 16:15:31 +0900

    style: Introduce.tsx & components  modify

--- src/app/Introduce/page.tsx
+++ src/app/Introduce/page.tsx
@@ -21,10 +21,6 @@ export default function Introduce() {
             paddingBottom: "20vh",
           }}
         >
-          {/* <TextBox
-            title="성장배경"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          /> */}
           <Contents
             titles={["성장과정", "성격", "자신의 장단점", "입사포부"]}
             contents={[
